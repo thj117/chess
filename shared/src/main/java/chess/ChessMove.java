82eda0b039f2713cd3f3a48459d36c215c74d764
Trevor <thj117@byu.edu>
2025-09-15 21:43:25 -0600

    I have finished the getting started video and need to still add all the possible movements.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -42,4 +42,9 @@ public class ChessMove {
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s%s", startPosition, endPosition);
+    }
 }
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -32,4 +32,9 @@ public class ChessPosition {
     public int getColumn() {
         return col;
     }
+
+    @Override
+    public String toString() {
+        return String.format("[%d,%d]", row, col);
+    }
 }
