37ded012e9c614999ff771af3004113b56ced46d
Trevor <thj117@byu.edu>
2025-09-15 18:26:24 -0600

    Binding construct parameters to fields

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,8 @@ package chess;
  */
 public class ChessPosition {
 
-    private final int row;
+    private final int row
+            ;
     private final int col;
 
     public ChessPosition(int row, int col) {
@@ -21,7 +22,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -29,6 +30,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
