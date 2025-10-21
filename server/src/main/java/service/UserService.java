9882ab36770f688c6ad2c4e932b8059caac8a870
Trevor <thj117@byu.edu>
2025-10-21 16:33:59 -0600

    finishing register result

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -19,6 +19,28 @@ public class UserService {
         if (req == null || req.username() == null || req.password() == null || req.email() == null){
             throw new IllegalArgumentException("bad request");
         }
-        return null;
+        UserData u = new UserData(req.username(), req.password(), req.email());
+
+        try {
+           dao.createUser(u);
+        } catch (DataAccessException except) {
+            // if duplicate user exists, translate
+            if (dao.getUser(u.username()).isPresent()) {
+                throw new DataAccessException("already taken");
+            }
+            throw except;
+        }
+
+        String token = generateToken();
+        AuthData a = new AuthData(token, u.username());
+        dao.createAuth(a);
+        return new RegisterResult(u.username(), token);
     }
+
+
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
 }
