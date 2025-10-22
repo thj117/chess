1c8911c30e0189ea92b5f662b75be9a59cf4e2b1
Trevor <thj117@byu.edu>
2025-10-21 18:10:31 -0600

    First attempt at registers endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,17 +2,44 @@ package server;
 
 import com.google.gson.Gson;
 import dataaccess.DataAccess;
+import dataaccess.InMemoryDataAccess;
+import dataaccess.DataAccessException;
 import io.javalin.*;
+import service.RegisterRequest;
+import service.RegisterResult;
+import service.UserService;
+
+import java.util.Map;
 
 public class Server {
 
     private final Javalin javalin;
     private final Gson gson = new Gson();
+    private final DataAccess dao = new InMemoryDataAccess();
+    private final UserService userService = new UserService(dao);
+
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        // Register
+        javalin.post("/user", ctx -> {
+            try {
+                RegisterRequest req = gson.fromJson(ctx.body(), RegisterRequest.class);
+                RegisterResult res = userService.register(req);
+                ctx.status(200).json(res);
+            } catch (IllegalArgumentException e) {
+                ctx.status(400).json(Map.of("message", "Error: bad request"));
+            } catch (DataAccessException e) {
+                if ("already taken".equals(e.getMessage())) {
+                    ctx.status(403).json(Map.of("message", "Error: already taken"));
+                } else {
+                    ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
+                }
+            } catch (Exception e) {
+                ctx.status(500).json(Map.of("message", "Error: " + e.getMessage()));
+            }
+        });
 
     }
 
