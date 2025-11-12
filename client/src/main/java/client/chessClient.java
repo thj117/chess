29219a858f86a4dedde2aa32f682399dcd98c163
Trevor <thj117@byu.edu>
2025-11-12 16:54:35 -0700

    setting up the post and pre login

--- client/src/main/java/client/chessClient.java
+++ client/src/main/java/client/chessClient.java
@@ -28,11 +28,27 @@ public class chessClient {
     }
 
     private void handlePreLogin(String input, Scanner scanner){
-
+        switch (input){
+            case "help" -> printPreHelp;
+            case "quit" -> {
+                System.out.println("Goodbye!");
+                running = false;
+            }
+            case "register" -> System.out.println("register");
+            case "login" -> System.out.println("login!");
+                default -> System.out.println("Unknown command, please type 'help' for a list of valid commands");
+        }
     }
 
     private void handlePostLogin(String input, Scanner scanner){
-
+        switch (input){
+            case "help" -> printPostHelp;
+            case "logout" -> System.out.println("logout");
+            case "create game" -> System.out.println("create game");
+            case "list games" -> System.out.println("list games");
+            case "play game" -> System.out.println("play game");
+            case "observe game" -> System.out.println("observe game");
+        }
     }
 
 }
